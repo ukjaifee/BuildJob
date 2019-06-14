608319e335b390114e942487ae5c6a53bf3035e3
ukjaifee@gmail.com <ukjaifee@gmail.com>
2019-06-14 07:14:49 +0530

    build job code

---
+++ src/main/java/com/maven/Build/BuildJob.java
@@ -0,0 +1,13 @@
+package com.maven.Build;
+
+import org.testng.annotations.Test;
+
+public class BuildJob {
+	
+	@Test
+	public void buildingJob() {
+		
+		System.out.println("I am building a build");
+	}
+
+}
